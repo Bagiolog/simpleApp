4619ff5c68efd570b93a3c5eee56c002a9aa84f2
Giacomo Bagiolo <bagiolog@gmail.com>
2018-09-24 12:33:03 +0200

    Login e PagerTabStrip

--- app/src/main/java/com/example/stage3/myapplication/home.java
+++ app/src/main/java/com/example/stage3/myapplication/home.java
@@ -14,22 +14,12 @@ import android.widget.Toolbar;
 
 import java.util.ArrayList;
 
-public class home extends AppCompatActivity implements albumUno.OnFragmentInteractionListener , albumDue.OnFragmentInteractionListener, albumTre.OnFragmentInteractionListener, impostazioni.OnFragmentInteractionListener {
+public class home extends AppCompatActivity implements albumUno.OnFragmentInteractionListener , albumDue.OnFragmentInteractionListener, albumTre.OnFragmentInteractionListener {
 
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        
-        final Button impostazioni=(Button)findViewById(R.id.impostazioni);
-        impostazioni.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
 
         ViewPager viewPager=(ViewPager)findViewById(R.id.viewPager);
         viewPager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager()));
